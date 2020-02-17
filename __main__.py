2a288434174065d969e0f6de1d2014dcd8f60377
Davis Engeler <davisengeler+github@gmail.com>
2020-02-17 03:23:34 -0600

    update for new project name

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import os
 import time
-import gs_price
-import gs_coms
-import gs_temp
+import g_price
+import g_coms
+import g_temp
 
 # =======================
 # Configuration
@@ -28,7 +28,7 @@ try:
 except Exception as e:
     last_heat = -1
     last_cool = -1
-    gs_coms.send_data(notification_url, ["Welcome to Griddle! If everything is set up right, I'll start saving "
+    g_coms.send_data(notification_url, ["Welcome to Griddle! If everything is set up right, I'll start saving "
                                          "you money by shifting your AC and Heat usage away from random spikes! "
                                          "Make sure that you're starting the container every minute for best results."])
 
@@ -39,11 +39,11 @@ print(f"{last_heat} {last_cool}")
 # =======================
 
 # Get the current price
-current_price, updated_at = gs_price.get_current(load_zone)
+current_price, updated_at = g_price.get_current(load_zone)
 print(f"Currently costs {round(current_price, 2)}¢ ({updated_at})")
 
 # Calculate the new temperature settings
-temp_offset = gs_temp.calculate_offset(current_price)
+temp_offset = g_temp.calculate_offset(current_price)
 new_cool = fav_cool + temp_offset
 new_heat = fav_heat - temp_offset
 
@@ -53,19 +53,19 @@ new_heat = fav_heat - temp_offset
 
 if last_cool != new_cool or last_heat != new_heat:
     # Update the heat
-    gs_coms.send_data(
+    g_coms.send_data(
         heat_url,
         [new_heat]
     )
     # Update the cool
     time.sleep(2)
-    gs_coms.send_data(
+    g_coms.send_data(
         cool_url,
         [new_cool]
     )
     # Send a notification about the changes
     time.sleep(2)
-    gs_coms.send_data(
+    g_coms.send_data(
         notification_url,
         [f"Griddle – The price of energy is at {current_price}¢ so I'm setting your temp to {new_heat}º – {new_cool}º."]
     )
